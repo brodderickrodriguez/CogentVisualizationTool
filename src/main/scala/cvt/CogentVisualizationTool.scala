5f14366b01ab9e8607b3ce0835c4f52c30ebce86
brodderick rodriguez <bcr@auburn.edu>
2018-07-05 17:21:24 -0500

    Initial Commit

---
+++ src/main/scala/cvt/CogentVisualizationTool.scala
@@ -0,0 +1,12 @@
+package cvt
+
+object CogentVisualizationTool {
+    
+    def main(args: Array[String]) : Unit = {
+        
+        println("hi, CVT")
+        
+        
+    } // main()
+    
+}
