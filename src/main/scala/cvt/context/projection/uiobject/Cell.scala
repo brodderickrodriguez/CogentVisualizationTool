8e0f39a972fa1d81891b5f8fa6c063893ead8b4c
brodderickrodriguez <bcr@brodderick.com>
2018-07-29 13:59:56 -0500

    Added ScalaDoc comments for UIObject.

--- src/main/scala/cvt/context/projection/uiobject/Cell.scala
+++ src/main/scala/cvt/context/projection/uiobject/Cell.scala
@@ -8,22 +8,30 @@ import cvt.context.projection.{Coordinate, Grid}
 /** @constructor A cell User Interface Object. This is used in the Grid Projection.
   * @param dimension the dimension of the cell in pixels.
   * @param _coordinate the x,y coordinate of where the Cell is on the Grid.
+  * @author Brodderick Rodriguez (bcr@brodderick.com)
+  * @since 29 July 2018
   */
 class Cell(dimension : Dimension, _coordinate: Coordinate, grid : Grid) extends UIObject {
     /** A getter for the Cell's coordinates. */
     val coordinate : Coordinate = _coordinate
     /** an ArrayBuffer containing pointers to all the AgentUI's in the cell. */
     val agents : ArrayBuffer[AgentUI] = new ArrayBuffer[AgentUI]()
-    
-    
+
+
+    /** Adds an AgentUI to this Cell.
+      * @param agent The Agent we wish to add to the Cell.
+      */
     def add(agent : AgentUI) : Unit = {
         // send a notification to all AgentUIs in the cell that there has been another AgentUI added
         sendNotificationToAgents(AgentUINotification.addedAgentToCell)
         // append to the list of AgentUIs in the cell
         agents += agent
     } // add()
-    
-    
+
+
+    /** Removes an Agent from this Cell.
+      * @param agent The Agent we wish to remove.
+      */
     def remove(agent : AgentUI) : Unit = {
         // remove from the list of agents
         agents -= agent
@@ -31,8 +39,9 @@ class Cell(dimension : Dimension, _coordinate: Coordinate, grid : Grid) extends 
         sendNotificationToAgents(AgentUINotification.removedAgentFromCell)
         // set the AgentUIs cell to null
     } // remove()
-    
-    
+
+
+    /** Removes all the Agents from the Cell. */
     def removeAllAgents() : Unit = agents.clear()
     
     
--- src/main/scala/cvt/context/projection/uiobject/UIObject.scala
+++ src/main/scala/cvt/context/projection/uiobject/UIObject.scala
@@ -1,13 +1,21 @@
 package cvt.context.projection.uiobject
 import java.awt.{Color, Dimension}
-
 import cvt.context.projection.Coordinate
 
+
+/** An Abstract class for all UI Objects. UI Objects are those which appear on screen.
+  * @author Brodderick Rodriguez (bcr@brodderick.com)
+  * @since 29 July 2018
+  */
 abstract class UIObject {
     var color : Color = _
     var dimension : Dimension = new Dimension(0, 0)
     var ID : Integer = -1
     var description : String = "[UIObject]"
     var absoluteLocation : Coordinate = new Coordinate(0 ,0)
+
+    /** Quickly finds the center of a UIObject's view.
+      * @return a Coordinate of the center of the UIObject.
+      */
     def center : Coordinate = absoluteLocation.add(new Coordinate(dimension.width / 2, dimension.height / 2))
 } // UIObject
